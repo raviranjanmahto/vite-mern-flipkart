193b402f821b020883ea66994cbbff11aed4e886
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-01-04 00:02:24 +0530

    userModel and route added

---
+++ server/model/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fName: {
+      type: String,
+      trim: true,
+      required: [true, "Please tell us your first name!"],
+    },
+    lName: {
+      type: String,
+      trim: true,
+      default: "N/A",
+    },
+    email: {
+      type: String,
+      required: [true, "Please tell us your email"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      minlength: [6, "Password must be at least 6 character"],
+      required: [true, "Password is required!"],
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
