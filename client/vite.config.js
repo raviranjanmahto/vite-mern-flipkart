49b474f877bce2da8221a1b8c471f288292e01d5
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-06-13 18:17:48 +0530

    output dir ite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this matches what is expected in vercel.json
+  },
   server: {
     open: true,
     proxy: {
