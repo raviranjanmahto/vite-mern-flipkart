01f0730f9d1086aa606a5982276457cb06cec859
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-06-21 17:58:59 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:7007",
-        changeOrigin: true,
-      },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
     },
+  },
+  server: {
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:7007",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
