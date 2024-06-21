3334c3d1bd36a3e5f407d7cf8c6400bf94ffba05
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-06-21 18:05:30 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-    },
-  },
   server: {
     // proxy: {
     //   "/api": {
