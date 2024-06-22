18835c3d509c26bffc81b828462adafa446b69a2
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-06-22 08:13:26 +0530

    vercel for all route

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URL = "https://raviranjan-flipkart-server.vercel.app";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const userApi = createApi({
   reducerPath: "userApi",
